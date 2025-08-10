26550923d67b4bc740ca7bb7e81992f8beeaa10a
PTTin1952 <tinptps1952@gmail.com>
2025-08-10 13:19:12 +0700

    Cập nhật Chi tiết đơn hàng

--- src/main/java/com/example/mini_supermarket/entity/ChiTietGioHang.java
+++ src/main/java/com/example/mini_supermarket/entity/ChiTietGioHang.java
@@ -1,13 +1,21 @@
 package com.example.mini_supermarket.entity;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "ChiTietGioHang", indexes = {
